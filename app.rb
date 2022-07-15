e1f9fb096bfa5ef16835597c15fa2c41d0f3ea12
sabih <sabihsaleh@yahoo.com>
2022-07-15 11:40:03 +0100

    Route '/Peeps' ERB file setup

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ class Chitter < Sinatra::Base
     erb :index
   end
   get '/peeps' do
-  peeps = ["Hello, I am peep", "I am also a peep", "Also a peep"]
+  @peeps = ["Hello, I am peep", "I am also a peep", "Also a peep"]
+  erb :peeps
   end
 
   run! if app_file == $0
